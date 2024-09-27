64f4c322741936371d6ed6d982fcfbefc61cd257
Lira7682 <a.23308051281191@cbtis128.edu.mx>
2024-09-27 17:47:19 -0600

    Add files via upload

---
+++ Lira_Hernandez_Dayana_Yamileth_1191_3W.4/lira_hernandez_dayana_yamileth_1191_3W.4.py
@@ -0,0 +1,16 @@
+print (" ") #agregar espacio
+print ("Lira Hernandez Dayana Yamileth: determina el perimetro y area de un rectangulo")
+print (" ") #agregar espacio
+
+base = int(input("Ingrese valor de la base")) #instruccion para agregar valor
+altura = int(input("Ingrese altura")) #instruccion para agregar valor
+
+perimetro = (base + altura) * 2 #operacion a realizar
+print ("El resultado es:", perimetro) #imprime resultado de la operacion
+
+base = int(input("Ingrese valor de la base")) #instruccion para agregar valor
+altura = int(input("Ingrese altura")) #instruccion para agregar valor
+
+area = base * altura #operacion a realizar
+print ("El resultado es:", area)#imprime resultado de la operacion
+print (" ") #agregar espacio
